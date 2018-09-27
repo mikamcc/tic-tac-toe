20a28c0a8cc03443596df1aa550abcd3f071f355
mika_mccarron <mikamccarron@gmail.com>
2018-09-27 12:49:10 +1000

    started AI function

--- js/main.js
+++ js/main.js
@@ -6,6 +6,43 @@ let currentTurn = 'X'; // let secondTurn = 'O'; I don't need this as it can flip
 let moveCount = 0;
 let xWin = 0;// store win of X
 let oWin = 0;// store win of O
+let board = [
+  null, null, null,
+  null, null, null,
+  null, null, null,
+];
+
+const aiCells = function(){
+  let freeSquareIDs = [];
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === null){
+      freeSquareIDs.push(i);//1
+    }
+  }
+  // console.log(freeSquareIDs);
+  return freeSquareIDs;//2
+
+}
+
+const playAIMove = function (currentPlayer) {
+  console.log('playAIMove()');
+  let availableCells = aiCells();//5
+  // console.log(availableCells);
+  let aiId = Math.floor(Math.random() * availableCells.length);//3
+  // console.log(aiId);
+  board[aiId] = currentPlayer;//4
+  console.log(board);
+  // find the free squares which the AI player can choose from for its move
+  // [
+  //   'X',  null, null,
+  //   null,  'O', null,
+  //   null, null,  'X',
+  // ];
+
+  // => output should be [1,2,3,5,6,7]
+
+
+};
 
 const checkForWin = function(){
 
@@ -19,29 +56,29 @@ const checkForWin = function(){
   const s7 = $('#7').text();
   const s8 = $('#8').text();
 
-    if (
-      (s0 === s1 && s0 === s2 && s0.length > 0) ||
-      (s3 === s4 && s3 === s5 && s3.length > 0) ||
-      (s6 === s7 && s6 === s8 && s6.length > 0) ||
-      (s0 === s4 && s0 === s8 && s0.length > 0) ||
-      (s2 === s4 && s2 === s6 && s2.length > 0) ||
-      (s2 === s4 && s2 === s6 && s2.length > 0) ||
-      (s0 === s3 && s0 === s6 && s0.length > 0) ||
-      (s1 === s4 && s1 === s7 && s1.length > 0) ||
-      (s2 === s5 && s2 === s8 && s2.length > 0)
-    ){
-        console.log('You win!');
-        $('#messageBox').text(`You win, ${ currentTurn }!`); //prints currentTurn X or O
-        gameStillInProgress = false;// game is over. user can't click anymore.
+  if (
+    (s0 === s1 && s0 === s2 && s0.length > 0) ||
+    (s3 === s4 && s3 === s5 && s3.length > 0) ||
+    (s6 === s7 && s6 === s8 && s6.length > 0) ||
+    (s0 === s4 && s0 === s8 && s0.length > 0) ||
+    (s2 === s4 && s2 === s6 && s2.length > 0) ||
+    (s2 === s4 && s2 === s6 && s2.length > 0) ||
+    (s0 === s3 && s0 === s6 && s0.length > 0) ||
+    (s1 === s4 && s1 === s7 && s1.length > 0) ||
+    (s2 === s5 && s2 === s8 && s2.length > 0)
+  ){
+    console.log('You win!');
+    $('#messageBox').text(`You win, ${ currentTurn } !`); //prints currentTurn X or O
+    gameStillInProgress = false;// game is over. user can't click anymore.
 
-        if (currentTurn === 'X') {
-          xWin++; // add 1 to the xWin
-          $('#xScore').text(xWin);//print score on X:
-        } else {
-          oWin++;
-          $('#oScore').text(oWin);//print score on O:
-        }
-      }
+    if (currentTurn === 'X') {
+      xWin++; // add 1 to the xWin
+      $('#xScore').text(xWin);//print score on X in html:
+    } else {
+      oWin++;
+      $('#oScore').text(oWin);//print score on O in html:
+    }
+  }
 };//checkForWin function
 
 $('.box').on('click', function (){
@@ -53,11 +90,19 @@ $('.box').on('click', function (){
   }
 
   moveCount = moveCount+1; // moveCount++;  // moveCount += 1;
-  console.log(moveCount);//everytime clicked count it as one
+  // console.log(moveCount);//everytime clicked count it as one
 
   // set the text contents of the DIV that was clicked,
   // using whatever is in the 'currentTurn' variable
   $(this).text( currentTurn );//this prints currentTurn X or O
+
+  // get the id of the box that was clicked (convert to integer)
+  const id =  parseInt( this.id );
+  // use the id as an index into our board array,
+  // to save the current player's symbol (X or O) into
+  // the rigt position
+  board[ id ] = currentTurn;//??
+  console.log(board);
 
   // use currentTurn ('X' or 'O') to set the class for the
   // clicked element, to give it a colour
@@ -65,7 +110,6 @@ $('.box').on('click', function (){
 
   checkForWin();
 
-  // playAIMove();
 
   if (currentTurn === 'X'){ // this will flip X & O
     currentTurn = 'O';
@@ -73,6 +117,7 @@ $('.box').on('click', function (){
     currentTurn = 'X';
   }
 
+  playAIMove(currentTurn);
 
   // The game is a draw if we get to this point and:
   // 1. gameStillInProgress is still true
@@ -94,11 +139,11 @@ $('#reset').on('click', function () {
   $('#messageBox').text('Tic Tac Toe');//reset the heading
 });
 
-$('#startAgain').on('click', function () {
+$('#clearScore').on('click', function () {
   $('.box').empty(); //clear all the text inside the contents
   $('.box').removeClass('O X'); // clear the colour classes
   moveCount = 0;//reset moveCount
   gameStillInProgress = true; //reseting
   $('#messageBox').text('Tic Tac Toe');//reset the heading
-  $('.score').empty().text(0);
+  $('.score').text(0);//reset to 0
 });
